e5000acd4d651dd3ba223e34fbcd127b865ffa3c
nadunchanna98 <gancp1218@gmail.com>
2022-09-15 01:59:35 +0530

    pages structure and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes,Route, Link} from "react-router-dom";
+import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 
 function App() {
-  return (
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+
+         
+          <Link to="/">Home Page</Link>
+          <Link to="/login">Login</Link>
+
+          <Routes>
+          <Route path="/" exact element={<Homepage/>} />
+          <Route path="/login" exact element={<Login/>} />
+          </Routes>
+          
+          
+        </Router>
     </div>
   );
 }
 
 export default App;
+
+
+//npm i axios 
+//npm i react-router-dom 
+//version 6 is deference
+//for get input use library formik
+//for vallidation -> npm i yup
